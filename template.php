6261de2dc42f06b4f6d5ee6d8d0aedf8f498a3d5
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-06-02 06:58:13 -0400

    Theme du front menu avec affichage de la description

--- template.php
+++ template.php
@@ -4,57 +4,15 @@
  * The primary PHP file for this theme.
  */
 
-/*function angole_menu_link($variables) {
-  $view = _menu_views_replace_menu_item($variables['element']);
-
+function angole_menu_link__menu_front_menu($variables) {
   $element = $variables['element'];
   $sub_menu = '';
 
   if ($element['#below']) {
-    // Prevent dropdown functions from being added to management menu so it
-    // does not affect the navbar module.
-    if (($element['#original_link']['menu_name'] == 'management') && (module_exists('navbar'))) {
-      $sub_menu = drupal_render($element['#below']);
-    }
-    elseif ((!empty($element['#original_link']['depth'])) && ($element['#original_link']['depth'] == 1)) {
-      // Add our own wrapper.
-      unset($element['#below']['#theme_wrappers']);
-      $sub_menu = '<ul class="dropdown-menu">' . drupal_render($element['#below']) . '</ul>';
-      // Generate as standard dropdown.
-      $element['#title'] .= ' <span class="caret"></span>';
-      $element['#attributes']['class'][] = 'dropdown';
-      $element['#localized_options']['html'] = TRUE;
-
-      // Set dropdown trigger element to # to prevent inadvertant page loading
-      // when a submenu link is clicked.
-      $element['#localized_options']['attributes']['data-target'] = '#';
-      $element['#localized_options']['attributes']['class'][] = 'dropdown-toggle';
-      $element['#localized_options']['attributes']['data-toggle'] = 'dropdown';
-    }
+    $sub_menu = drupal_render($element['#below']);
   }
-  // On primary navigation menu, class 'active' is not set on active menu item.
-  // @see https://drupal.org/node/1896674
-  if (($element['#href'] == $_GET['q'] || ($element['#href'] == '<front>' && drupal_is_front_page())) && (empty($element['#localized_options']['language']))) {
-    $element['#attributes']['class'][] = 'active';
-  }
-
-  // si on a une vue, c'est elle qu'on met en dessous
-    if ($view !== FALSE) {
-        if (!empty($view)) {
-          $sub_menu = $view;
-          $element['#title'] .= ' <span class="caret"></span>';
-          $element['#attributes']['class'][] = 'dropdown';
-          $element['#localized_options']['html'] = TRUE;
-
-          // Set dropdown trigger element to # to prevent inadvertant page loading
-          // when a submenu link is clicked.
-          $element['#localized_options']['attributes']['data-target'] = '#';
-          $element['#localized_options']['attributes']['class'][] = 'dropdown-toggle';
-          $element['#localized_options']['attributes']['data-toggle'] = 'dropdown';
-
-        }
-    }
-
   $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  $output .= '<div class="description">' . $element['#original_link']['localized_options']['attributes']['title'] . '</div>';
   return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
-}*/
+}
+
